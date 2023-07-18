07adb74a6dea8c04f69619d88dd8cbfcc3626363
senseiX21 <mahdix2118@protonmail.com>
2023-07-18 11:37:19 +0100

    Updated create review

--- dist/app/modules/comments/review.controller.js
+++ dist/app/modules/comments/review.controller.js
@@ -17,18 +17,31 @@ const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
 const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
 const http_status_1 = __importDefault(require("http-status"));
 const review_service_1 = require("./review.service");
+const config_1 = __importDefault(require("../../../config"));
+const jwthelpers_1 = require("../../../helpers/jwthelpers");
+const user_model_1 = require("../users/user.model");
+const ApiError_1 = __importDefault(require("../../../errors/ApiError"));
 //create a new Review
 const createReview = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.params.id;
     const data = req.body;
-    const result = yield review_service_1.ReviewService.createReview(data, id);
-    (0, sendResponse_1.default)(res, {
-        success: true,
-        statusCode: http_status_1.default.OK,
-        message: 'Review created successfully',
-        data: result
-    });
-    next();
+    const token = req.headers.authorization;
+    const decoded = jwthelpers_1.jwtHelpers.verifyToken(token, config_1.default.jwt.secret);
+    const user = decoded.userId;
+    const isUserExist = yield user_model_1.User.findById(user);
+    if (isUserExist) {
+        const result = yield review_service_1.ReviewService.createReview(data, id);
+        (0, sendResponse_1.default)(res, {
+            success: true,
+            statusCode: http_status_1.default.OK,
+            message: 'Review created successfully',
+            data: result
+        });
+        next();
+    }
+    else {
+        throw new ApiError_1.default(http_status_1.default.NOT_FOUND, 'Unauthorized user');
+    }
 }));
 //get reviews
 const getReviews = (0, catchAsync_1.default)((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
--- src/app/modules/comments/review.controller.ts
+++ src/app/modules/comments/review.controller.ts
@@ -1,25 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { RequestHandler } from "express";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
 import { ReviewService } from "./review.service";
+import config from "../../../config";
+import { jwtHelpers } from "../../../helpers/jwthelpers";
+import { Secret } from "jsonwebtoken";
+import { User } from "../users/user.model";
+import ApiError from "../../../errors/ApiError";
 
 
 //create a new Review
 const createReview: RequestHandler = catchAsync(async (req, res, next) => {
     const id = req.params.id;
     const data = req.body;
-    const result = await ReviewService.createReview(data, id);
 
-    sendResponse(
-        res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: 'Review created successfully',
-        data: result
-    });
+    const token: any = req.headers.authorization;
+    const decoded = jwtHelpers.verifyToken(token, config.jwt.secret as Secret)
+    const user = decoded.userId;
+    const isUserExist = await User.findById(user)
 
-    next();
+
+
+    if (isUserExist) {
+        const result = await ReviewService.createReview(data, id);
+
+        sendResponse(
+            res, {
+            success: true,
+            statusCode: httpStatus.OK,
+            message: 'Review created successfully',
+            data: result
+        });
+
+        next();
+
+    } else {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Unauthorized user');
+    }
+
 });
 
 //get reviews
